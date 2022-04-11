e8f38b90fbf3cb1726a4f7bcbccb3dea4fd29ae4
Cindy Muraya <cmuraya12@gmail.com>
2022-04-11 11:54:15 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints numbers between 0 to 99
- * Return: 0 if successful
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
