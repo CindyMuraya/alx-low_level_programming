7e2cb0601dd4f99b4c54b595e7990eccaf8d1f19
Cindy Muraya <cmuraya12@gmail.com>
2022-04-11 04:58:04 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - determines either greater than 5, is less than 6, or is 0
- * Return: Always 0 if successful
+ * main - Determines either greater than 5, is less than 6, or is 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
