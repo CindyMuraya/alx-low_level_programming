d55d1f22f802f85ff5a83e25aac3c77d2b0e0842
Cindy Muraya <cmuraya12@gmail.com>
2022-04-25 02:28:01 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string
+ * Return: pointer to the string
+ */
+char *leet(char *s)
+{
+	int indx1 = 0, indx2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (s[indx1])
+	{
+		for (indx2 = 0; indx2 <= 7; indx2++)
+		{
+			if (s[indx1] == leet[indx2] ||
+					s[indx1] - 32 == leet[indx2])
+				s[indx1] = indx2 + '0';
+		}
+		indx1++;
+	}
+	return (s);
+}
