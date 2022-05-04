5bc5ce451643c90b44daa3b9ca86291b589fe86a
Cindy Muraya <cmuraya12@gmail.com>
2022-05-04 02:28:59 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: command line arguments
+ * @argv: array of pointers to the arguments
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
