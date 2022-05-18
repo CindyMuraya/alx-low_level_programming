5f5e55082dc942c8401482a7f669e3e67125d766
Cindy Muraya <cmuraya12@gmail.com>
2022-05-18 12:30:07 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+
 /**
  * print_char - display character
  * @arg: arguments
