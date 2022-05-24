d2dacc9a880e9c8bdad6f05877eb603c1098c48b
Cindy Muraya <cmuraya12@gmail.com>
2022-05-24 13:19:31 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,19 +14,13 @@ size_t print_list(const list_t *h)
 
 	nodes = 0;
 
-
-
-
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
-
 			h = h->next;
 			nodes++;
-
-
 		}
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
