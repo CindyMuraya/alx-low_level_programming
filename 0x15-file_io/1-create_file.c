0f4d9614facffdf2939fddb6abe45a258dc43c34
Cindy Muraya <cmuraya12@gmail.com>
2022-06-02 05:52:25 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,9 +10,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, file;
+	int o, w, len;
 
-	file = 0;
+	len = 0;
 
 	if (filename == NULL)
 	{
@@ -21,8 +21,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (file = 0; text_content[file];)
-			file++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
@@ -32,6 +32,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+
 	close(o);
 
 	return (1);
