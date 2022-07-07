5ed71b0b6acb87666efc56054886a121101e7b19
Cindy Muraya <cmuraya12@gmail.com>
2022-07-07 09:52:31 -0700

    task 1

---
+++ 1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ * @h: head of dlistint_t list
+ *
+ * Return: the number of elements in a linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes;
+
+	nodes = 0;
+
+	while (h)
+	{
+		nodes;
+		h = h->next;
+	}
+
+	return (nodes);
+}
