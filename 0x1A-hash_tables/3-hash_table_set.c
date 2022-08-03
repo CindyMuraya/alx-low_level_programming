32f95c7b4e34d01e775e7f6153e4030a17988d0c
Cindy Muraya <cmuraya12@gmail.com>
2022-08-03 02:18:51 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,15 +16,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	{
 		return (0);
-	}
 
 	value_copy = strdup(value);
 	if (value_copy == NULL)
-	{
 		return (0);
-	}
 
 	index = key_index((const unsigned char *)key, ht->size);
 	for (i = index; ht->array[i]; i++)
